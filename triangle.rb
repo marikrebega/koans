f7819b33c1ca3d687e224075a6cb3680f3563d5e
Мар'ян Ребега <marikrebega@gmail.com>
2020-05-25 17:23:40 +0300

    fixed about_triangle_project_2 and edit triangle.rb file

--- triangle.rb
+++ triangle.rb
@@ -14,6 +14,9 @@
 #   about_triangle_project_2.rb
 #
 def triangle(a, b, c)
+  raise TriangleError if [a,b,c].min <= 0
+  x, y, z = [a,b,c].sort
+  raise TriangleError if x + y <= z
   case [a,b,c].uniq.size
   when 1 then :equilateral
   when 2 then :isosceles
